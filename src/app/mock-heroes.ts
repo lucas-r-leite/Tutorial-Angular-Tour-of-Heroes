63d96a0eabb20e55b113b0605e318e25dc01b13c
genergia <sistema.web@genergia.com.br>
2021-03-15 14:04:11 -0300

    Add Service - 5

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Bono Vox'},
+    {id:2, name: 'Stuart Piu II'},
+    {id:3, name: 'Bodao'},
+    {id:4, name: 'Rafael'},
+    {id:5, name: 'Stuartzilla'},
+    {id:6, name: 'King Bo'}
+
+];
