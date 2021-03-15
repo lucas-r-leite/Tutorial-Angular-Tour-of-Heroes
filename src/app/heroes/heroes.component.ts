fd78a151ac8f674f7478c549c63fc06e9b4eccc2
genergia <sistema.web@genergia.com.br>
2021-03-15 14:56:14 -0300

    Parte 4 do tutorial finalizada

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-//import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
 
-  //heroes = HEROES;
-  heroes: Hero[] = [];
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -23,13 +23,11 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-
-    //this.heroes = this.heroService.getHeroes();//Busca as informações de forma síncrona
-    getHeroes(): void {
-      this.heroService.getHeroes()
-          .subscribe(heroes => this.heroes = heroes);
-      //this.heroes = this.heroService.getHeroes();//Busca as informações de forma síncrona
-    }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
