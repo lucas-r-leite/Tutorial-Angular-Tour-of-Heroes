8fca52cdb72cddd8accb4bc9c2afd0fe6bb11759
genergia <sistema.web@genergia.com.br>
2021-03-15 17:01:57 -0300

    Tentativa 5.2.2 do tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,6 +9,8 @@ import { MessageService } from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
